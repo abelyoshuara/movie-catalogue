ac168fde610b10de544117e24dd14498e8defec3
Abel Yoshuara <abelyoshuara@gmail.com>
2023-03-10 09:37:32 +0800

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: '45bcd41e1be64fdcf266becb67e6b696',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+}
+
+export default CONFIG
